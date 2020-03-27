0d3b344731a3a618c268f52cb45a71680d767c30
rene <renerecalde2@gmail.com>
2020-03-27 08:45:19 -0300

    Update lists

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from "react";
 import {
     HydraAdmin,
-    ResourceGuesser
+    ResourceGuesser,
+    ListGuesser,
+    FieldGuesser
 } from "@api-platform/admin";
 import authProvider from './authProvider';
 import MyLoginPage from './MyLoginPage';
 
 const entrypoint = process.env.REACT_APP_API_ENTRYPOINT;
+
+
+const CountryList = props => (
+    <ListGuesser {...props}>
+        <FieldGuesser source={"code"} />
+        <FieldGuesser source={"name"} />
+    </ListGuesser>
+);
+
+const ProvinceList = props => (
+    <ListGuesser {...props}>
+        <FieldGuesser source={"code"} />
+        <FieldGuesser source={"name"} />
+        <FieldGuesser source={"abbreviation"} />
+        <FieldGuesser source={"country"} />
+    </ListGuesser>
+);
+
+
+const CityList = props => (
+    <ListGuesser {...props}>
+        <FieldGuesser source={"name"} />
+        <FieldGuesser source={"createdAt"} />
+        <FieldGuesser source={"updatedAt"} />
+        <FieldGuesser source={"province"} />
+    </ListGuesser>
+);
+
+const ZoneList = props => (
+    <ListGuesser {...props}>
+        <FieldGuesser source={"code"} />
+        <FieldGuesser source={"name"} />
+        <FieldGuesser source={"type"} />
+        <FieldGuesser source={"city"} />
+    </ListGuesser>
+);
 
 export default props => (
     <HydraAdmin entrypoint={ entrypoint } authProvider={ authProvider } loginPage = {MyLoginPage}>
@@ -19,10 +57,10 @@ export default props => (
         <ResourceGuesser name="stores" />
         <ResourceGuesser name="people" />
         <ResourceGuesser name="addresses" />
-        <ResourceGuesser name="zones" />
-        <ResourceGuesser name="cities" />
-        <ResourceGuesser name="provinces" />
-        <ResourceGuesser name="countries" />
+        <ResourceGuesser name="zones"  list={ZoneList}/>
+        <ResourceGuesser name="cities" list={CityList} />
+        <ResourceGuesser name="provinces" list={ProvinceList} />
+        <ResourceGuesser name="countries" list={CountryList}  />
     </HydraAdmin>
 
 );
