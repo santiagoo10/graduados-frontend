3ffc65490a11e340b01a3a989496f91a5e6082a8
santiago bolzico <santiago.bolzico@mercadolibre.com>
2020-10-11 21:52:59 -0300

    fixed dataprovider

--- src/SmartComponent/cities.js
+++ src/SmartComponent/cities.js
@@ -1,33 +1,64 @@
 import React from "react";
-import { CreateGuesser, ShowGuesser, FieldGuesser, ListGuesser, EditGuesser, InputGuesser} from "@api-platform/admin";
+import {
+  CreateGuesser,
+  ShowGuesser,
+  FieldGuesser,
+  ListGuesser,
+  EditGuesser,
+  InputGuesser,
+} from "@api-platform/admin";
 
-export const CityList = props => (
-    <ListGuesser {...props} title={"Ciudades"}>
-        <FieldGuesser source={"name"} label={"Nombre"}/>
-        <FieldGuesser source={"province"} label={"Provincia"}/>
-    </ListGuesser>
+import {
+  ReferenceInput,
+  AutocompleteInput,
+  TextField,
+  ReferenceField,
+} from "react-admin";
+
+export const CityList = (props) => (
+  <ListGuesser {...props} title={"Ciudades"}>
+    <FieldGuesser source={"name"} label={"Nombre"} />
+    <ReferenceField label="Provincia" source="province" reference="provinces">
+      <TextField source="name" />
+    </ReferenceField>
+  </ListGuesser>
 );
 
-export const CityShow = props => (
-    <ShowGuesser {...props} title={"Ver ciudad"}>
-        <FieldGuesser source={"name"} label={"Nombre"} addLabel={true} />
-        <FieldGuesser source={"province"} label={"Provincia"} addLabel={true} />
-        <FieldGuesser source={"createdAt"} label={"Creado"} addLabel={true} />
-        <FieldGuesser source={"updatedAt"} label={"Actualizado"} addLabel={true} />
-    </ShowGuesser>
+export const CityShow = (props) => (
+  <ShowGuesser {...props} title={"Ver ciudad"}>
+    <FieldGuesser source={"name"} label={"Nombre"} addLabel={true} />
+    <ReferenceField label="Provincia" source="province" reference="provinces">
+      <TextField source="name" />
+    </ReferenceField>
+    <FieldGuesser source={"createdAt"} label={"Creado"} addLabel={true} />
+    <FieldGuesser source={"updatedAt"} label={"Actualizado"} addLabel={true} />
+  </ShowGuesser>
 );
 
-export const CityEdit = props => (
-    <EditGuesser {...props} title={"Editar ciudad"}>
-        <InputGuesser source={"name"} label={"Nombre"}/>
-        <InputGuesser source={"province"} label={"Provincia"}/>
-    </EditGuesser>
+export const CityEdit = (props) => (
+  <EditGuesser {...props} title={"Editar ciudad"}>
+    <InputGuesser source={"name"} label={"Nombre"} />
+    <ReferenceInput
+      source="province"
+      reference="provinces"
+      label="Provincia"
+      filterToQuery={(searchText) => ({ name: searchText })}
+    >
+      <AutocompleteInput optionText="name" />
+    </ReferenceInput>
+  </EditGuesser>
 );
 
-
-export const CityCreate = props => (
-    <CreateGuesser {...props} title={"Crear ciudad"}>
-        <InputGuesser source={"name"} label={"Nombre"}/>
-        <InputGuesser source={"province"} label={"Provincia"}/>
-    </CreateGuesser>
+export const CityCreate = (props) => (
+  <CreateGuesser {...props} title={"Crear ciudad"}>
+    <InputGuesser source={"name"} label={"Nombre"} />
+    <ReferenceInput
+      source="province"
+      reference="provinces"
+      label="Provincia"
+      filterToQuery={(searchText) => ({ name: searchText })}
+    >
+      <AutocompleteInput optionText="name" />
+    </ReferenceInput>
+  </CreateGuesser>
 );
